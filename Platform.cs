f1a4c919579b49f9548c6555f685280cf744c252
ivanenich <egor.ivanenko.06@bk.ru>
2025-11-19 03:09:24 +0300

    feat: add .

--- Platform.cs
+++ Platform.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization;
 namespace Ivanenko01._03
 {
     /// <summary>
-    /// Класс для представления платформы
+    /// Класс для представления платформы.
     /// </summary>
     [Serializable]
     [DataContract]
